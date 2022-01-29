09bab4c8e19f3fc0168542ef47ecc3e7170773c1
SirawitN <nine_sirawit@hotmail.com>
2022-01-29 11:56:07 +0700

    Hospital basic Rest API

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+//route file
+const hospitals = require("./routes/hospitals");
+
+//Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/v1/hospitals", hospitals);
+
+app.listen(
+  PORT,
+  console.log(
+    `Server's running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
