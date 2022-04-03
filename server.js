012b930013c715b205365036c4c459cca119177a
SirawitN <nine_sirawit@hotmail.com>
2022-04-03 14:29:51 +0700

    OpenAPI and Swagger

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const helmet = require("helmet");
 const xss = require("xss-clean");
 const express = require("express");
 const connectDB = require("./config/db");
+const swaggerJsDoc = require("swagger-jsdoc");
 const cookieParser = require("cookie-parser");
+const swaggerUI = require("swagger-ui-express");
 const rateLimiter = require("express-rate-limit");
 const mongoSanitize = require("express-mongo-sanitize");
 
@@ -13,6 +15,20 @@ const mongoSanitize = require("express-mongo-sanitize");
 const hospitals = require("./routes/hospitals");
 const auth = require("./routes/auth");
 const appointments = require("./routes/appointments");
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library APIs",
+      version: "1.0.0",
+      description: "A simple Express VacQ API",
+    },
+    servers: [{ url: "http://localhost:3000/api/v1" }],
+  },
+  apis: ["./routes/*.js"],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -40,7 +56,7 @@ app.use(helmet());
 app.use(xss());
 
 //Rate limiting
-app.use(limiter);
+// app.use(limiter);
 
 //Prevent http param pollution
 app.use(hpp());
@@ -51,6 +67,8 @@ app.use(cors());
 app.use("/api/v1/hospitals", hospitals);
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/appointments", appointments);
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 const server = app.listen(
   PORT,
